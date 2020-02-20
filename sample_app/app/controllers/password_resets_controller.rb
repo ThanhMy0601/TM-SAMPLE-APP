87a7358d5958f9263a4851c733063931277efa20
sharon-pham-goldenowl <sharon.pham.goldenowl@gmail.com>
2020-02-20 09:34:05 +0700

    add project

---
+++ sample_app/app/controllers/password_resets_controller.rb
@@ -0,0 +1,69 @@
+class PasswordResetsController < ApplicationController
+  before_action :get_user, only: [:edit, :update]
+  before_action :valid_user, only: [:update]
+  before_action :check_expiration, only: [:update] #case1
+
+  def new
+  end
+
+  def create
+    # byebug
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = "Email sent with password reset instructions"
+      redirect_to root_url
+    else
+      flash.now[:danger] = "Email address not found"
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    # byebug
+    if params[:user][:password].empty? #case3
+      @user.errors.add(:password, "can't be empty")
+      render 'edit'
+    elsif @user.update_attributes(user_params) #case4
+      log_in @user
+      flash[:success] = "Password has been reset."
+      redirect_to @user
+    else
+      render 'edit' #case2
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
+    #before filters
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    # Confirms a valid user.
+    def valid_user
+      # byebug
+      unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id])) # && @user.authenticated?(:reset, params[:id])
+        # flash[]
+        redirect_to root_url
+      end
+    end
+
+    # checks expiration of reset tokenf.
+    def check_expiration
+      # byebug
+      if @user.password_reset_expired?
+        flash[:danger] = "Password reset has expired."
+        redirect_to new_password_reset_url
+      end
+    end
+end
